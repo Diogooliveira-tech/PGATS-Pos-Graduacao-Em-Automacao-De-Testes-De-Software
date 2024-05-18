df8bc3e4cc984a6d4521ef872d517fdc6126b399
Diogo <diogooliveira.792@gmail.com>
2024-05-18 11:38:19 -0300

    ajuste arquivo controle Aulas

--- aula1.js
+++ aula1.js
@@ -16,3 +16,13 @@ nome = "Diogo";
 
 // Apresento o conteúdo da variável no Console
 console.log(`Meu nome é ${nome} e a Sandra é a minha esposa, a CEO da casa`);
+
+// Dentro do JS para criar uma variavél de coisas que valem dentro do contexto atual ou contextos internos, devo usar o LET.  
+// Isso significa que estou criando uma caixa onde "let nome = "Diogo";
+// Essa minha caixa eu chamei ela de "nome", o sinal de "igual =" significa que ele "recebe" algo, nesse caso o valor "Diogo"   
+// Então estou estou falando que a variavel "nome" definida como sendo do tipo let, recebe o texto "Diogo".
+// let nome, declara uma variavel nome
+// let resultado, declara uma variavel resultado
+// let mensagem, declara uma variavel mensagem
+
+// template string em javascript, basta usar duas crases " `` ". Eu posso fazer assim console.log(`Meu nome é ${nome} e a Dani é minha`);
