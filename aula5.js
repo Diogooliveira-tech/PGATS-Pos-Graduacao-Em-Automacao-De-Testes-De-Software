ba3e80b54704273cbee0ea34b9dacee2b0f62315
Diogo <diogooliveira.792@gmail.com>
2024-06-24 22:51:05 -0300

    continuação das aulas modulo 4 exercicio1 imc

--- aula5.js
+++ aula5.js
@@ -1,3 +1,53 @@
+// Declaração de Variaveis
+const primeiroNumero = 5;
+const segundoNumero = 7;
+
+// Cálculo da Soma
+function somar(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+const resultadoSoma = somar(primeiroNumero, segundoNumero)
+
+// Cálculo da Média
+    const resultadoMedia = resultadoSoma / 2; 
+
+// Exibição do Resultado
+//console.log(resultadoMedia)
+console.log(`O resultado é ${resultadoMedia}`);
+
+// parei 2:03
+
+/*
+function resultadoMedia() {
+    const valor1 = 5;
+    const valor2 = 7;
+
+    else()
+}
+*/
+
+
+
+
 //FUNÇÕES 
 
 //IMPORTANTE! 1:41 AULA 4
+
+//Pontos vistos em aula
+// if 
+// console.log
+// declaração de variaveis
+// let
+// var 
+// const
+// else
+// else if
+// declaração de tipos 
+// vetores
+// operadores matematicos
+// objetos "estudar mais"
+// matrizes
+// parametro (conceito dentro de functions que temos a oportunidade de passar uma "variavel/valor" para dentro e ser usado/disponivel dentro de um bloquinho de código)
+// template string
+
